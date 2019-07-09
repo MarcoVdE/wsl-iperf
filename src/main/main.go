3e1f545cdf15222538244145d8d40689fd4017d0
Marco Van der Eecken <marco@valkyriweb.com>
2019-07-09 16:08:05 +0200

    GUI into tabs, powershell refactor, WIP.

--- src/main/main.go
+++ src/main/main.go
@@ -3,43 +3,138 @@ package main
 import (
 	"../powershell"
 	"fmt"
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
+	"log"
+	"strconv"
 )
 
 func main() {
+	//TODO: Convert to tabbed container, other option: terminate main window on opening iPerf3 and starting new main window.
 	fmt.Println("starting the project")
 
 	main_app := app.New()
 
-	w := main_app.NewWindow("iPerf 3 - WSL")
+	w := main_app.NewWindow("The Windows Linux Network Tool")
 
+	//iPerf Tab labels and widgets.
+	iPerf3AddressLabel := widget.NewLabelWithStyle("Address: ", fyne.TextAlignLeading, fyne.TextStyle{})
+	//iPerf3AddressLabel := widget.NewLabel("Address: ")
 	iPerf3AddressWidget := widget.NewEntry()
+	iPerf3AddressWidget.SetPlaceHolder("ping.online.net")
+
+	iPerf3PortLabel := widget.NewLabel("Port: ")
 	iPerf3PortWidget := widget.NewEntry()
+	iPerf3PortWidget.SetPlaceHolder("5001")
+
+	iPerf3SpeedLabel := widget.NewLabel("Speed (Mbps): ")
 	iPerf3SpeedWidget := widget.NewEntry()
+	iPerf3SpeedWidget.SetPlaceHolder("80")
 
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Welcome to the automated iPerf tool for Windows WSL1"),
-		widget.NewButton("Enable WSL", func() {
-			EnableWSL()
-		}),
-		widget.NewButton("Install Ubuntu on WSL", func() {
-			InstallUbuntu()
-		}),
-		widget.NewButton("Install iPerf3 on WSL", func() {
-			InstallIPerf3()
-		}),
-		iPerf3AddressWidget,
-		iPerf3PortWidget,
-		iPerf3SpeedWidget,
-		widget.NewButton("Run iPerf3 Tests", func() {
-			go powershell.RunIperf3(iPerf3AddressWidget.Text, iPerf3PortWidget.Text, iPerf3SpeedWidget.Text)
-		}),
-		widget.NewButton("Quit", func() {
-			main_app.Quit()
-		}),
-	))
+	iPerf3VerboseWidget := widget.NewCheck("Verbose: ", nil) //Check fyne-io/widget/check_test.go:14
 
+	iPerf3ReverseWidget := widget.NewCheck("Reverse (download): ", nil) //Check fyne-io/widget/check_test.go:14
+
+	iPerf3TimeLabel := widget.NewLabel("Time (seconds): ")
+	iPerf3TimeWidget := widget.NewEntry()
+	iPerf3TimeWidget.SetPlaceHolder("10")
+
+	iPerf3OmitLabel := widget.NewLabel("Omit (seconds): ")
+	iPerf3OmitWidget := widget.NewEntry()
+	iPerf3OmitWidget.SetPlaceHolder("3")
+
+	iPerf3isUDPWidget := widget.NewCheck("UDP: ", nil) //Check fyne-io/widget/check_test.go:14
+
+	iPerf3Output := widget.NewMultiLineEntry()
+	iPerf3Output.SetReadOnly(true)
+	iPerf3Output.SetPlaceHolder("Output Window. \n\n\nPress Test to start test")
+
+	iPerfLayout := fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+		iPerf3AddressLabel, iPerf3AddressWidget,
+		iPerf3PortLabel, iPerf3PortWidget,
+		iPerf3SpeedLabel, iPerf3SpeedWidget,
+		iPerf3VerboseWidget, iPerf3ReverseWidget,
+		iPerf3TimeLabel, iPerf3TimeWidget,
+		iPerf3OmitLabel, iPerf3OmitWidget,
+		iPerf3isUDPWidget,
+	)
+
+	w.SetContent(
+		widget.NewVBox(
+
+			widget.NewTabContainer(
+				widget.NewTabItem("Home",
+					widget.NewVBox(
+						widget.NewLabel("Welcome to the automated iPerf tool for Windows WSL1"),
+						widget.NewButton("Enable WSL", func() {
+							EnableWSL()
+						}),
+						widget.NewButton("Install Ubuntu on WSL", func() {
+							InstallUbuntu()
+						}),
+						widget.NewButton("Install iPerf3 on WSL", func() {
+							InstallIPerf3()
+						}),
+					),
+				),
+				widget.NewTabItem("iPerf Test",
+					widget.NewVBox(
+						iPerfLayout,
+						iPerf3Output,
+						widget.NewButton("Run Test", func() {
+							//get ints and test valid.
+							bandwidth, err := strconv.Atoi(iPerf3SpeedWidget.Text)
+							if err != nil {
+								log.Printf("Bandiwdth Error: %s", err)
+							}
+							port, err := strconv.Atoi(iPerf3SpeedWidget.Text)
+							if err != nil {
+								log.Printf("Port Conversion Error: %s", err)
+							}
+							time, err := strconv.Atoi(iPerf3SpeedWidget.Text)
+							if err != nil {
+								log.Printf("Time Conversion Error: %s", err)
+							}
+							omit, err := strconv.Atoi(iPerf3SpeedWidget.Text)
+							if err != nil {
+								log.Printf("Time Conversion Error: %s", err)
+							}
+
+							//TODO: rewrite the result as channel bringing back info and ticking the update every 0.3s
+							powershell.RunIPerf3Test(powershell.NewIPerfObject(iPerf3AddressWidget.Text, port,
+								bandwidth, iPerf3VerboseWidget.Checked, iPerf3ReverseWidget.Checked,
+								time, omit, iPerf3isUDPWidget.Checked))
+
+						}),
+						widget.NewButton("Copy Result", func() {
+							//get the result from output window.
+							if iPerf3Output.Text != "" {
+								clipboard := fyne.Clipboard(w.Clipboard())
+								clipboard.SetContent(iPerf3Output.Text)
+							} else {
+								widget.NewModalPopUp(widget.NewLabel("No text to copy"), w.Canvas())
+								//TODO: close modal.
+							}
+						}),
+					),
+				),
+
+				widget.NewTabItem("ISP",
+					widget.NewVBox(
+						widget.NewLabelWithStyle("Default ISP tests", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+						widget.NewButton("Atomic CT FF", func() {}),
+						widget.NewButton("Cool Ideas - JHB", func() {}),
+						widget.NewButton("Cool Ideas - CT", func() {}),
+					),
+				),
+			),
+
+			widget.NewButton("Quit", func() {
+				main_app.Quit()
+			}),
+		)) //end of Main VBox and window content.
 	w.ShowAndRun()
 }
 
--- src/powershell/powershell.go
+++ src/powershell/powershell.go
@@ -1,10 +1,10 @@
 package powershell
 
-
 import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	ps "github.com/bhendo/go-powershell"
@@ -39,7 +39,7 @@ func EnableWSL() {
 	fmt.Println("Enabling WSL")
 	//startPowerShellSession()
 	pscommand := "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux"
-	out, err := exec.Command("cmd", "powershell -command \"" + pscommand + "\"").Output()
+	out, err := exec.Command("cmd", "powershell -command \""+pscommand+"\"").Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -53,7 +53,7 @@ func InstallUbuntuWSL() {
 	fmt.Println("Installing Ubuntu WSL")
 	fmt.Println("Initializing download")
 	pscommand := "Invoke-WebRequest -Uri https://aka.ms/" + appName + " -OutFile Ubuntu.appx -UseBasicParsing"
-	out, err := exec.Command("cmd", "powershell -command \"" + pscommand  + "\"").Output()
+	out, err := exec.Command("cmd", "powershell -command \""+pscommand+"\"").Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -63,7 +63,7 @@ func InstallUbuntuWSL() {
 
 	fmt.Println("Starting install of image")
 	pscommand = "Add-AppxPackage .\\" + appName
-	out, err = exec.Command("cmd", "powershell -command \"" + pscommand  + "\"").Output()
+	out, err = exec.Command("cmd", "powershell -command \""+pscommand+"\"").Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -71,7 +71,7 @@ func InstallUbuntuWSL() {
 	fmt.Printf("The output is %s\n", out)
 	fmt.Println("Install of image ended, starting initialization")
 	pscommand = "bash"
-	out, err = exec.Command("cmd", "powershell -command \"" + pscommand  + "\"").Output()
+	out, err = exec.Command("cmd", "powershell -command \""+pscommand+"\"").Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -86,7 +86,7 @@ func InstallIPerf3WSL() {
 	pscommand[0] = "bash"
 	pscommand = append(pscommand, "sudo apt-get install iperf3")
 
-	out, err := exec.Command("cmd", "powershell -command \"" + strings.Join(pscommand, ";")  + "\"").Output()
+	out, err := exec.Command("cmd", "powershell -command \""+strings.Join(pscommand, ";")+"\"").Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -125,6 +125,94 @@ func InstallIPerf3WSL() {
 //	cmd.Wait()
 //}
 
+func runWSLPowershellScript(args []string) string {
+	back := &backend.Local{}
+
+	// start a local powershell process
+	shell, err := ps.New(back)
+	if err != nil {
+		panic(err)
+	}
+	defer shell.Exit()
+
+	pscommand := make([]string, 1)
+	pscommand[0] = "wsl.exe bash -c \""
+	pscommand = append(pscommand, args...)
+	//pscommand = append(pscommand, "\" 2>&1 | %{\"$_\"}") //to output to console instead of error object
+	pscommand = append(pscommand, "\"") //needed for closing
+
+	stdout, stderr, err := shell.Execute(strings.Join(pscommand, ""))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+
+	} else {
+		//noinspection GoNilness
+		return strings.Join([]string{stdout, stderr, err.Error()}, "")
+	}
+}
+
+type iPerfObject struct {
+	address   string //-c client
+	port      int
+	bandwidth int //target bandwidth, default tcp unlimited, udp 1 Mbps. AKA speed, shouldn't exceed 80% of line speed.
+
+	//flags
+	verbose bool
+	reverse bool //Server sends, client receives, default: true
+	time    int  //seconds to transmit for
+	omit    int  //how many seconds to omit for for TCP slow start, default: 2
+	isUdp   bool //udp or tcp, default false
+}
+
+func NewIPerfObject(address string, port int, bandwidth int, verbose bool, reverse bool, time int, omit int, isUdp bool) iPerfObject {
+	return iPerfObject{address: address, port: port, bandwidth: bandwidth, verbose: verbose, reverse: reverse, time: time, omit: omit, isUdp: isUdp}
+}
+
+func iPerfTest(object iPerfObject) string {
+	iPerfTest := []string{"iperf3"}
+	iPerfTest = append(iPerfTest, "-4") //IPv4
+	if (iPerfObject{}.address != "") {
+		iPerfTest = append(iPerfTest, "--reverse")
+	} else {
+		log.Println("failure, no address given")
+		return "Failed iPerf Test, no address given"
+	}
+	if (iPerfObject{}.port != 0) { //default port is 5001, but rare
+		iPerfTest = append(iPerfTest, " --port "+strconv.Itoa(iPerfObject{}.port))
+	} else {
+		iPerfTest = append(iPerfTest, " --port 5001 ")
+	}
+	if (iPerfObject{}.bandwidth != 0) { //default port is 5001, but rare
+		iPerfTest = append(iPerfTest, " --bandwidth "+strconv.Itoa(iPerfObject{}.bandwidth))
+	}
+	if (iPerfObject{}.verbose) {
+		iPerfTest = append(iPerfTest, " --verbose ")
+	}
+	if (iPerfObject{}.reverse) { //reverse = client receives/download test
+		iPerfTest = append(iPerfTest, "--reverse")
+	}
+	if (iPerfObject{}.time != 0) {
+		iPerfTest = append(iPerfTest, "--time "+strconv.Itoa(iPerfObject{}.time))
+	}
+	if (iPerfObject{}.omit != 0) {
+		iPerfTest = append(iPerfTest, "--omit "+strconv.Itoa(iPerfObject{}.omit))
+	}
+	if (iPerfObject{}.isUdp) { //if false, default is tcp, no addition to command for iPerf3
+		iPerfTest = append(iPerfTest, "--udp ")
+	}
+
+	return runWSLPowershellScript([]string{strings.Join(iPerfTest, " ")}) //note the space in Join separator.
+}
+
+func RunIperf3Output(address string, port string, speed string) string {
+	return runWSLPowershellScript([]string{"iperf3 -4 -V -R -t 5 -O 3 -u -b " + speed + "M -c " + address + " -p " + port})
+}
+
+func RunIPerf3Test(i iPerfObject) {
+	iPerfTest(i)
+}
+
 func RunIperf3(address string, port string, speed string) {
 
 	//fmt.Print(backend.Local{})
@@ -134,7 +222,7 @@ func RunIperf3(address string, port string, speed string) {
 	// start a local powershell process
 	shell, err := ps.New(back)
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
 	defer shell.Exit()
 
@@ -148,7 +236,7 @@ func RunIperf3(address string, port string, speed string) {
 	//if err != nil {
 	//	panic(err)
 	//}
-	stdout, stderr, err := shell.Execute("wsl.exe bash -c \"iperf3 -4 -V -R -t 5 -O 3 -u -b " + speed + "M -c " + address +" -p " + port +"\" 2>&1 | %{\"$_\"}")
+	stdout, stderr, err := shell.Execute("wsl.exe bash -c \"iperf3 -4 -V -R -t 5 -O 3 -u -b " + speed + "M -c " + address + " -p " + port + "\" 2>&1 | %{\"$_\"}")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -159,7 +247,6 @@ func RunIperf3(address string, port string, speed string) {
 	//if err != nil {
 	//	panic(err)
 	//}
-
 
 	fmt.Println(stdout, " stderr:", stderr)
 }
